31827f6a6cd6bb365dc52b9a345d35367104ba7e
codedev1992 <142015642+codedev1992@users.noreply.github.com>
2023-12-31 09:21:48 +0530

    Update _3_Design_UI.py

--- _3_Design_UI.py
+++ _3_Design_UI.py
@@ -11,17 +11,17 @@ time1_str = st.text_input("please put later time here like 17:34:06")
 time2_str = st.text_input("please put earlier time here like 12:50:05")
 
 time1_str = time1_str.replace(".",":")
-time2_str = time1_str.replace(".",":")
+time2_str = time2_str.replace(".",":")
 
 time1_str = time1_str.replace("-",":")
-time2_str = time1_str.replace("-",":")
+time2_str = time2_str.replace("-",":")
 
 submit = st.button("Compute Difference")
 if submit:
     # Convert the string times to datetime objects
     time1 = datetime.strptime(time1_str, time_format)
     time2 = datetime.strptime(time2_str, time_format)
-
+    print(time1, time2)
     # Calculate the time difference
     time_difference = time1 - time2
 
